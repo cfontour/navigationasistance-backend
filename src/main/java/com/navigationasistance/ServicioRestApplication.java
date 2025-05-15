7a21395246a9710d338a42a08584dc47e2aaac40
carlos <cfontour@gmail.com>
2025-05-15 14:07:50 -0300

    Forzar escaneo de controladores

--- src/main/java/com/navigationasistance/ServicioRestApplication.java
+++ src/main/java/com/navigationasistance/ServicioRestApplication.java
@@ -3,10 +3,11 @@ package com.navigationasistance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.navigationasistance")
 public class ServicioRestApplication {
 
 	public static void main(String[] args) {
+		System.out.println(">>> INICIANDO APLICACIÓN SPRING");
 		SpringApplication.run(ServicioRestApplication.class, args);
 	}
 
