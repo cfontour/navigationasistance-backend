3e6eb7868193143403eca0d9aef091f34072333f
carlos <cfontour@gmail.com>
2025-07-17 21:04:30 -0300

    Geotraser: regatas 2025 7

--- src/main/java/com/navigationasistance/mapper/UsuariocaPuntosControlRowMapper.java
+++ src/main/java/com/navigationasistance/mapper/UsuariocaPuntosControlRowMapper.java
@@ -16,6 +16,7 @@ public class UsuariocaPuntosControlRowMapper implements RowMapper<UsuariocaPunto
         u.setNadadorrutaId(rs.getString("nadadorruta_id"));
         u.setPuntoControl(rs.getString("punto_control"));
         u.setFechaHora(rs.getObject("fecha_hora", LocalDateTime.class));
+        u.setRutaId(rs.getInt("ruta_id")); // Asumiendo que ruta_id es un INT en tu DB
         return u;
     }
 }
