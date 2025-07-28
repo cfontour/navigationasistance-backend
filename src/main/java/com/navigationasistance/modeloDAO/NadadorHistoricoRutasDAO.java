8aa662df3e58061dfe5427b790657a6ad2c7ec2a
carlos <cfontour@gmail.com>
2025-07-28 16:31:14 -0300

    Geotraser: Traza en vivo Regatas

--- src/main/java/com/navigationasistance/modeloDAO/NadadorHistoricoRutasDAO.java
+++ src/main/java/com/navigationasistance/modeloDAO/NadadorHistoricoRutasDAO.java
@@ -75,9 +75,13 @@ public class NadadorHistoricoRutasDAO implements NadadorHistoricoRutasInterface 
 
     @Override
     public List<UUID> obtenerUltimoRecorrido(String usuarioId, LocalDate fecha) {
+        //String sql = "SELECT recorrido_id FROM public.nadadorhistoricorutas " +
+        //        "WHERE usuario_id = ? AND nadadorfecha = ? " +
+        //        "ORDER BY nadadorfecha DESC, nadadorhora DESC " + // <-- ¡ESTE ES EL CAMBIO CRUCIAL!
+        //        "LIMIT 1;";
         String sql = "SELECT recorrido_id FROM public.nadadorhistoricorutas " +
                 "WHERE usuario_id = ? AND nadadorfecha = ? " +
-                "ORDER BY nadadorfecha DESC, nadadorhora DESC " + // <-- ¡ESTE ES EL CAMBIO CRUCIAL!
+                "ORDER BY nadadorfecha DESC" +
                 "LIMIT 1;";
         return template.query(sql, new Object[]{usuarioId, fecha},
                 (rs, rowNum) -> UUID.fromString(rs.getString("recorrido_id")));
