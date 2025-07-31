f79e14306ee180e4299f6ed9870b35723322569a
carlos <cfontour@gmail.com>
2025-07-31 10:06:42 -0300

    Geotraser: Fecha de la zona en nadadorposicion 4

--- src/main/java/com/navigationasistance/modeloDAO/NadadorposicionDAO.java
+++ src/main/java/com/navigationasistance/modeloDAO/NadadorposicionDAO.java
@@ -51,7 +51,7 @@ public class NadadorposicionDAO implements NadadorposicionInterface {
                 "nadadorlat = EXCLUDED.nadadorlat, " +
                 "nadadorlng = EXCLUDED.nadadorlng, " +
                 "bearing = EXCLUDED.bearing, " + // ✅ Añade esto para actualizar el bearing
-                "fecha_ultima_actualizacion = EXCLUDED.fecha_ultima_actualizacion";
+                "fecha_ultima_actualizacion = EXCLUDED.fecha_ultima_actualizacion"; //va la fecha
         return template.update(sql, n.getUsuarioid(), n.getNadadorlat(), n.getNadadorlng(), n.getBearing(), n.getFechaUltimaActualizacion());
     }
 
